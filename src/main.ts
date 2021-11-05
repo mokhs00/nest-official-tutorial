c43595d604548c76f7bed156065284ecbcc6d015
mokhs00 <mokhs00@naver.com>
2021-11-05 16:52:15 +0900

    add global middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { functionalLogger } from 'middleware/functionalLogger.middleware';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(functionalLogger);
   await app.listen(3000);
 }
 bootstrap();
